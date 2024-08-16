f0c6a34f0d7206c844a915cc00e9df147c8ffe0f
SandeepDinodia <151106767+SandeepDinodia@users.noreply.github.com>
2024-08-16 10:35:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const buttons=document.querySelectorAll('.btn');
+const stop=document.querySelector('.stop');
+
+let audio;
+buttons.forEach((button)=>{
+	button.addEventListener("click", function(){
+	     audio=new Audio(`sounds${button.textContent}.mp3`)
+	    audio.play();
+	});
+	
+});
+stop.addEventListener("click",()=>{
+	if(audio){
+		audio.pause();
+	}
+});
